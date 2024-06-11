2a477cb19a67848817e79db2bfb900c083822261
UbdaNam <abdugreat2@gmail.com>
2024-06-11 17:19:43 +0300

    Add responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+
 function App() {
-  return <h1>Game Vault</h1>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={'nav'}>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area={'aside'}>Aside</GridItem>
+      </Show>
+      <GridItem area={'main'}>Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
